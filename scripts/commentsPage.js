3f6a5633e761151647e1056de1c1b8d7ad44face
Nick <nikitalytvynov0506@gmail.com>
2023-01-03 12:30:15 +0200

    workingShit

--- scripts/commentsPage.js
+++ scripts/commentsPage.js
@@ -10,13 +10,13 @@ class CommentsPage {
         this.url = url;
         this.comments = [];
         this.page = 0;
+        this.deletePage = 5;
         this.limit = 5;
         this.renderForm();
-        this.listContainer = document.createElement('div')
+        this.listContainer = document.createElement('div');
         this.root.append(this.listContainer);
         this.fetchList();
         this.renderButton();
-        this.previousPage();
     }
 
     renderStructure() {
@@ -28,15 +28,17 @@ class CommentsPage {
         this.commentsPageList = document.createElement('div');
         this.formContainer.classList.add('form-container');
         this.commentsPageList.classList.add('comments-page__list')
-        const onSuccessCallback =  (newComment) => {
+        const onSuccessCallback = (newComment) => {
             console.log(newComment);
             this.listContainer.innerHTML = ``;
             this.comments.push();
             this.comments.unshift(newComment);
             this.renderList();
+            this.deleteRenderList();
             // додаті до массіва комментс
             // свторіті метод якій буде додаваті один Comment
         }
+
         this.form = new AddComentForm(this.formContainer, this.url, onSuccessCallback);
         this.root.append(this.formContainer);
     }
@@ -44,7 +46,6 @@ class CommentsPage {
     renderOne(commentData) {
         console.log('render one')
         const item = new Comment(this.listContainer, commentData);
-
     }
 
     fetchList() {
@@ -52,6 +53,7 @@ class CommentsPage {
         fetch(`${this.url}?_order=desc&_sort=id&_limit=${this.limit}&_page=${this.page}`).then((res) => res.json()).then(data => {
             this.comments = data;
             this.renderList();
+            this.deleteRenderList();
         })
     }
 
@@ -60,9 +62,16 @@ class CommentsPage {
         for(let i = 0; i < this.comments.length; i++) {
             const item = new Comment(this.listContainer, this.comments[i]);  
         }
-
     }
 
+    deleteRenderList() {
+        console.log('TUT NAPIST LOGICU DLYA UDALENIA SPISKA', this.page);
+        for(let i = 5; i > this.comments.length; i--) {
+            const item = new Comment(this.listContainer, this.comments[i]);
+        }
+    }
+
+    // написать функцию типо рендерлист на отнимания
 
     nextPage() {
         this.page += 1;
@@ -75,21 +84,22 @@ class CommentsPage {
     };
 
     previousPage() {
-        this.page - 1;
+        this.deletePage = -1;
         fetch(`${this.url}?_order=desc&_sort=id&_limit=${this.limit}&_page=${this.page}`).then((res) => res.json()).then(data => {
             this.comments = data;
-            this.renderList();
-    })
-}
+            this.listContainer.innerHTML = ``;
+            this.deleteRenderList();  
+        })
+    console.log('previous page', this.page);
+    }
 
     renderButton() {
         this.loadBtn = new BtnForLoad(this.root, API_URL, () => {
         this.nextPage();
         });
         this.clearBtn = new BtnForClear(this.root, API_URL, () => {
-
-        })  
+        this.previousPage();
+        });  
     }
-
 }
 
